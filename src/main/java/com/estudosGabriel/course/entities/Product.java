6b9c5130fd33b878e6d16b803ba45a48833f96ee
juliogabriel96s <juliogabrielsetor96@gmail.com>
2025-11-27 15:11:06 -0300

    first commit, created the entity product

---
+++ src/main/java/com/estudosGabriel/course/entities/Product.java
@@ -0,0 +1,104 @@
+package com.estudosGabriel.course.entities;
+
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public class Product implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	//Basic atributes
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	
+	private String nome;
+	
+	@jakarta.validation.constraints.NotNull
+	@Min(0)
+	private Double preco;
+	
+	@NotNull
+	@Min(0)
+	private Double estoque;
+	
+	// Constructors
+	
+	public Product() {
+		
+	}
+	
+	// getters and setters
+
+	public Product(Long id, String nome, @NotNull @Min(0) Double preco, @NotNull @Min(0) Double estoque) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+		this.estoque = estoque;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Double getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(Double estoque) {
+		this.estoque = estoque;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+}
